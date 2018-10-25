ba48f460501e1d4c8b395c11cf84cb2903f98656
海南海辰科技有限公司 <1837439788@qq.com>
2018-10-25 17:51:52 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+        <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+    </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
+export default {
+    components:{
+        FooterGuide
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    #app
+        width 100%
+        height 100%
+        max-width 640px
+        min-width 320px
+        background-color #f5f5f5
+        margin 0 auto
+</style>
